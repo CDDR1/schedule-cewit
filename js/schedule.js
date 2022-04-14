eb2b57841784aa605f926770184b8f214a2418f2
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-04-14 10:09:00 -0400

    Adding popup

--- js/schedule.js
+++ js/schedule.js
@@ -3,96 +3,123 @@ const timeSlots = document.querySelectorAll('.grid-hour');
 const gridItems = document.getElementsByClassName('grid-item');
 const submitButton = document.getElementById('submit-form');
 let daysPicked = [];
-let blockTakenList = [];  
+let blockTakenList = [];
 
 const player1 = {
-  courseInput: document.querySelector('#courseTitle'),
-  colorInput: document.querySelector('#colorPicker'),
-  dropDownMenu: document.querySelector('#time-slots'),
-  locationInput: document.querySelector('.location-input'),
-  professorInput: document.querySelector('.professor-input')
+    courseInput: document.querySelector('#courseTitle'),
+    colorInput: document.querySelector('#colorPicker'),
+    dropDownMenu: document.querySelector('#time-slots'),
+    locationInput: document.querySelector('.location-input'),
+    professorInput: document.querySelector('.professor-input'),
 };
 
-function reset(){
-  for (let p of [player1]){
-    p.courseInput.value = '';
-    p.colorInput.value = '';
-    p.locationInput.value = '';
-    p.professorInput.value = '';
-    p.dropDownMenu.value = '';
-    daysChecked.forEach((box) => (box.checked = false));
-    daysPicked = [];  
-  }
-}
-function emtpyInput(){
-  if(player1.dropDownMenu.value === '' || player1.courseInput.value === ''){
-    return true;
-  } else{
-    return false;
-  }
+const hidePopup = () => {
+    // Hides the popup's background
+    document.querySelector(':root').style.setProperty('--popup-bg-display', 'none');
+
+    // Hides the popup
+    document.querySelector('.popup').classList.remove('show-popup');
 }
 
-function findBlock(days){
-  //if true, there is repeating time block
-  let index = 0;
-  let placement = 0;
-  for (let gridItem of gridItems){
-    if(gridItem.innerText === player1.dropDownMenu.value){
-      for (let i of days){
-        placement = index + i + 1;
-        if(gridItems[placement].innerText != ''){ // finds a repeating block
-          return true;
-        }
-      }
-      return false;
+const showPopup = (content) => {
+    // Shows the popup's background
+    document.querySelector(':root').style.setProperty('--popup-bg-display', 'block');
+
+    // Shows the popup
+    document.querySelector('.popup').classList.add('show-popup');
+
+    // Populate popup's content 
+    document.querySelector('.popup-content').innerText = content;
+
+    const closePopupBtn = document.querySelector('.close-popup-btn');
+    const gotItBtn = document.querySelector('.gotit-btn');
+
+    closePopupBtn.addEventListener('click', hidePopup);
+    gotItBtn.addEventListener('click', hidePopup);
+};
+
+function reset() {
+    for (let p of [player1]) {
+        p.courseInput.value = '';
+        p.colorInput.value = '';
+        p.locationInput.value = '';
+        p.professorInput.value = '';
+        p.dropDownMenu.value = '';
+        daysChecked.forEach((box) => (box.checked = false));
+        daysPicked = [];
     }
-  }
+}
+function emtpyInput() {
+    if (player1.dropDownMenu.value === '' || player1.courseInput.value === '') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function findBlock(days) {
+    //if true, there is repeating time block
+    let index = 0;
+    let placement = 0;
+    for (let gridItem of gridItems) {
+        if (gridItem.innerText === player1.dropDownMenu.value) {
+            for (let i of days) {
+                placement = index + i + 1;
+                if (gridItems[placement].innerText != '') {
+                    // finds a repeating block
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 }
 
 submitButton.addEventListener('click', (e) => {
-  // we need to check for the following:
-  //  course title 
-  //  time slot matches
-  //  day matches
+    // we need to check for the following:
+    //  course title
+    //  time slot matches
+    //  day matches
 
-  e.preventDefault();
+    e.preventDefault();
 
-  // Fill in array (daysChecked) for the days chosen.
-  daysChecked.forEach((checkbox) => 
-  {
-    if (checkbox.checked) 
-    {
-      daysPicked.push(true);
-    } 
-    else 
-    {
-      daysPicked.push(false);
+    // Fill in array (daysChecked) for the days chosen.
+    daysChecked.forEach((checkbox) => {
+        if (checkbox.checked) {
+            daysPicked.push(true);
+        } else {
+            daysPicked.push(false);
+        }
+    });
+
+    let index = 0;
+    let placement = 0;
+    for (let gridItem of gridItems) {
+        // checks for input
+        if (emtpyInput()) {
+            // alert("You are missing either course name or time");
+            const popupContent = 'You are missing either course title or time';
+            showPopup(popupContent);
+            break;
+        }
+        // finds the time block
+
+        if (gridItem.innerText === player1.dropDownMenu.value) {
+            for (let i = 0; i < daysPicked.length; i++) {
+                // finds the days picked
+                placement = index + i + 1; // where the actual block is
+                if (daysPicked[i]) {
+                    //daysPicked[i] findBlock(daysChecked) === false)
+                    gridItems[placement].style.backgroundColor =
+                        player1.colorInput.value;
+                    gridItems[
+                        placement
+                    ].innerText = `${player1.courseInput.value}\n${player1.locationInput.value}\n${player1.professorInput.value}`;
+                }
+                // end of nested if statement
+            }
+        }
+        index++;
     }
-  });
-
-  let index = 0;
-  let placement = 0;
-  for (let gridItem of gridItems){
-    // checks for input
-    if (emtpyInput()){
-      alert("You are missing either course name or time");
-      break;
-    }
-    // finds the time block
-
-    if (gridItem.innerText === player1.dropDownMenu.value) {
-      for (let i = 0; i < daysPicked.length; i++) { 
-        // finds the days picked
-        placement = index + i + 1; // where the actual block is
-        if (daysPicked[i]){ //daysPicked[i] findBlock(daysChecked) === false)
-          gridItems[placement].style.backgroundColor = player1.colorInput.value;
-          gridItems[placement].innerText = 
-          `${player1.courseInput.value}\n${player1.locationInput.value}\n${player1.professorInput.value}`;
-        } 
-        // end of nested if statement
-      }
-    }
-    index++;
-  }
-  reset();
+    reset();
 });
